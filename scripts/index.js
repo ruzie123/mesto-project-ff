17d54e9c8a42b2a687fb489d6e939f016eff5cb7
Danil Gorbunov <astrakt16@yandex.ru>
2025-01-11 20:23:48 +0300

    add js code

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(Data, cardDelete) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    
+    cardElement.querySelector('.card__image').src = Data.link;
+    cardElement.querySelector('.card__image').alt = Data.name;
+    cardElement.querySelector('.card__title').textContent = Data.name;
+
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    deleteButton.addEventListener('click', function() {
+        cardDelete(cardElement);
+    });
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderCards(Cards) {
+    Cards.forEach(function (Data) {
+        const cardElement = createCard(Data, deleteCard);
+        cardContainer.append(cardElement);
+    });
+}
+
+renderCards(initialCards)
