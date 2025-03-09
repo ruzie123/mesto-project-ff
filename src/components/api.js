471494b12bc914ac59760f8da42b9628bb690588
Danil Gorbunov <astrakt16@yandex.ru>
2025-03-09 23:51:02 +0300

    fix: fix apiConfig

--- src/components/api.js
+++ src/components/api.js
@@ -1,8 +1,8 @@
 const apiConfig = {
   mainHost: "https://mesto.nomoreparties.co/v1/wff-cohort-33",
-  headersConfig: {
-    token: "6218e83b-95d0-497f-9115-5a732a7605e7",
-    ContentType: "application/json"
+  headers: {
+    authorization: "6218e83b-95d0-497f-9115-5a732a7605e7",
+    "Content-Type": "application/json"
   }
 }
 
@@ -15,27 +15,20 @@ export const getResponseData = (res) => {
 
 export const getProfileData = () => {
   return fetch(`${apiConfig.mainHost}/users/me`, {
-    headers: {
-      authorization: apiConfig.headersConfig.token,
-    },
+    headers: apiConfig.headers,
   }).then(getResponseData)
 };
 
 export const getCards = () => {
   return fetch(`${apiConfig.mainHost}/cards`, {
-    headers: {
-      authorization: apiConfig.headersConfig.token,
-    },
+    headers: apiConfig.headers,
   }).then(getResponseData)
 };
 
 export const saveUpdateProfile = (name, description) => {
   return fetch(`${apiConfig.mainHost}/users/me`, {
     method: "PATCH",
-    headers: {
-      authorization: apiConfig.headersConfig.token,
-      "Content-Type": apiConfig.headersConfig.ContentType,
-    },
+    headers: apiConfig.headers,
     body: JSON.stringify({
       name: name,
       about: description,
@@ -46,10 +39,7 @@ export const saveUpdateProfile = (name, description) => {
 export const addNewCard = (cardName, link) => {
   return fetch(`${apiConfig.mainHost}/cards`, {
     method: "POST",
-    headers: {
-      authorization: apiConfig.headersConfig.token,
-      "Content-Type": apiConfig.headersConfig.ContentType,
-    },
+    headers: apiConfig.headers,
     body: JSON.stringify({
       name: cardName,
       link: link,
@@ -60,41 +50,28 @@ export const addNewCard = (cardName, link) => {
 export const removeCard = (cardId) => {
   return fetch(`${apiConfig.mainHost}/cards/${cardId}`, {
     method: "DELETE",
-    headers: {
-      authorization: apiConfig.headersConfig.token,
-      "Content-Type": apiConfig.headersConfig.ContentType
-    }
+    headers: apiConfig.headers,
   }).then(getResponseData)
 }
 
 export const putLike = (cardId) => {
   return fetch(`${apiConfig.mainHost}/cards/likes/${cardId}`, {
     method: "PUT",
-    headers: {
-      authorization: apiConfig.headersConfig.token,
-      "Content-Type": apiConfig.headersConfig.ContentType
-    }
+    headers: apiConfig.headers,
   }).then(getResponseData)
 }
 
 export const deleteLike = (cardId) => {
   return fetch(`${apiConfig.mainHost}/cards/likes/${cardId}`, {
     method: "DELETE",
-    headers: {
-      authorization : apiConfig.headersConfig.token,
-      "Content-Type": apiConfig.headersConfig.ContentType
-      
-    }
+    headers: apiConfig.headers,
   }).then(getResponseData)
 };
 
 export const updateAvatar = (avatar) => {
   return fetch(`${apiConfig.mainHost}/users/me/avatar`, {
     method: "PATCH",
-    headers: {
-      authorization: apiConfig.headersConfig.token,
-      "Content-Type": apiConfig.headersConfig.ContentType
-    },
+    headers: apiConfig.headers,
     body: JSON.stringify({
       avatar: avatar
     }),
